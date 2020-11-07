f2d88d38ec11572f142a2528fe8fb27750e06545
Kyle <kylespinaio@hotmail.com>
2020-11-06 21:26:21 -0500

    added date function to calculate current date based on dday, month, arrays.

--- weather/src/App.js
+++ weather/src/App.js
@@ -5,6 +5,21 @@ import '../src/index.css';
 const baseURL = "api.openweathermap.org/data/2.5/weather?q="
 
 function App() {
+
+const dateBuilder = (d) => {
+ let months = ["January", "February", "March", "April", "May", "June", "July", 
+ "August", "September", "October", "November", "December"];
+ let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+let day = days[d.getDay()];
+let date = d.getDate();
+let month = months[d.getMonth()];
+let year = d.getFullYear()
+
+return `${day} ${date} ${month} ${year}`
+}
+
+
   return (
     <div className="app">
      <main className="main">
@@ -14,6 +29,10 @@ function App() {
          className = "search-bar"
          placeholder = "Search...">
          </input>
+       </div>
+       <div className = "location-box">
+        <div className = "location">Jacksonville</div>
+        <div className = "date">{dateBuilder(new Date())}</div>
        </div>
      </main>
     </div>
