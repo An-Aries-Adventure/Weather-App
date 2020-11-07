25dd5139fa584a1ad7b7e9190843106d4d4ec2df
Kyle <kylespinaio@hotmail.com>
2020-11-06 21:12:05 -0500

    added css styling to search bar and box

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import React from 'react';
+import '../src/index.css';
+
+
+const baseURL = "api.openweathermap.org/data/2.5/weather?q="
 
 function App() {
   return (
-    <div className="App">
-     hello
+    <div className="app">
+     <main className="main">
+       <div className="search-box">
+         <input
+         type = "text"
+         className = "search-bar"
+         placeholder = "Search...">
+         </input>
+       </div>
+     </main>
     </div>
   );
 }
